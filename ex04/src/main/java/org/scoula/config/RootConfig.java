9983ef1b29f3748676b4f13c74b452235403a278
Roa Kim <roa5108@gmail.com>
2025-06-11 14:07:15 +0900

    0611

---
+++ ex04/src/main/java/org/scoula/config/RootConfig.java
@@ -0,0 +1,57 @@
+package org.scoula.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource({"classpath:/application.properties"})
+@MapperScan(basePackages = {"org.scoula.mapper"})
+public class RootConfig {
+    @Value("${jdbc.driver}") String driver;
+    @Value("${jdbc.url}") String url;
+    @Value("${jdbc.username}") String username;
+    @Value("${jdbc.password}") String pw;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(pw);
+
+        HikariDataSource ds = new HikariDataSource(config);
+        return ds;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        return (SqlSessionFactory) sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        DataSourceTransactionManager manager= new DataSourceTransactionManager(dataSource());
+        return manager;
+    }
+
+}
